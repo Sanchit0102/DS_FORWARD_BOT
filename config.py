bc76db79dc01790c95fcdcedaad4086465667cf8
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-07-07 19:03:22 +0530

    Added FSub

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+import re
+import os
 from os import environ 
+
+id_pattern = re.compile(r'^.\d+$')
 
 class Config:
     API_ID = environ.get("API_ID", "25570025")
@@ -8,6 +12,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1562935405').split()]
+    AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 class temp(object): 
     lock = {}
