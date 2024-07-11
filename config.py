3f212bfd89be312d3c8df8d46966eea00efb01d2
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-07-11 21:16:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     API_ID = environ.get("API_ID", "25570025")
     API_HASH = environ.get("API_HASH", "62c95df09ad28778f17035b76abb3b22")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7201333775:AAHMuLKX4AHuP-YtMnjyquNqy153Kmf3GXE") 
-    BOT_SESSION = environ.get("BOT_SESSION", "public") 
+    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = environ.get("DATABASE_NAME", "publicforward-bot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1562935405').split()]
