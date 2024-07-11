86bd4d836fa8a7997904f489b7242722724fffea
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-07-11 21:09:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -7,7 +7,8 @@ from platform import python_version
 from translation import Translation
 from pyrogram import Client, filters, enums, __version__ as pyrogram_version
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaDocument
-from pyrogram.errors import *
+# from pyrogram.errors import *
+from pyrogram.errors import UserNotParticipant
 
 main_buttons = [[
         InlineKeyboardButton('🙋‍♂️ ʜᴇʟᴘ', callback_data='help'),
@@ -16,6 +17,37 @@ main_buttons = [[
         InlineKeyboardButton('⚙️ sᴇᴛᴛɪɴɢs ⚙️', callback_data='settings#main')
         ]]
 
+#========================[ FORCE SUB 2 ]================================#
+
+async def not_subscribed(_, client, message):
+    await db.add_user(client, message)
+    if not Config.FORCE_SUB:
+        return False
+    try:             
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
+        if user.status in {enums.ChatMemberStatus.BANNED, enums.ChatMemberStatus.LEFT}:
+            return True 
+        else:
+            return False                
+    except UserNotParticipant:
+        pass
+    return True
+
+
+@Client.on_message(filters.private & filters.create(not_subscribed))
+async def forces_sub(client, message):
+    buttons = [[InlineKeyboardButton(text="🤖 Join Update Channel 🤖", url=f"https://t.me/{Config.FORCE_SUB}") ]]
+    text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
+    try:
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
+        if user.status == enums.ChatMemberStatus.BANNED:                                   
+            return await client.send_message(message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")  
+        elif user.status == enums.ChatMemberStatus.LEFT:
+            return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+    except UserNotParticipant:                       
+        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+        
 #========================[ FORCE SUB DEF ]==============================#
 
 async def is_subscribed(bot, query, channel):
