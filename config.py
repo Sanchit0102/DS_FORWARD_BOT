95161b6015c9620f6731a2cf445a8eb7a4f405d5
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-07-31 13:29:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,13 +11,6 @@ class Config:
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://rasy46765:uxDpwzp7DDHIKnCj@cluster0.rakmbj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-2bot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1562935405').split()]
-    ADMIN = BOT_OWNER_ID
-    FORCE_SUB = environ.get('FORCE_SUB', '-1002158291506') # ⚠️ Required
-    AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
-    FORCE_SUB) else None
-    WEBHOOK = bool(environ.get("WEBHOOK", True))
-    PORT = int(environ.get("PORT", "8080"))
-    MUST_JOIN = environ.get("MUST_JOIN", "BOT_TESTING_OFFICIAL")
 
 class temp(object): 
     lock = {}
