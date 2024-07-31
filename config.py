2b26cde99e43ab7a9baf199b3e4adfc297105c92
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-07-31 12:47:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ id_pattern = re.compile(r'^.\d+$')
 class Config:
     API_ID = environ.get("API_ID", "21701469")
     API_HASH = environ.get("API_HASH", "ae456165cc04a86ee38bd8ced7dae0e3")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7017050619:AAE99kOJhQcbcsLAJNuWgHnJNsLZG4h72hM") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "6362281925:AAECiTn-SgatjUR6p3r0RlZQaMil3OYS_ak") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://rasy46765:uxDpwzp7DDHIKnCj@cluster0.rakmbj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-2bot")
