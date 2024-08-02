dfb0b4c9a1bb7d43302091cfa1cbf21ab2d93795
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-08-02 16:30:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ id_pattern = re.compile(r'^.\d+$')
 class Config:
     API_ID = environ.get("API_ID", "25570025")
     API_HASH = environ.get("API_HASH", "62c95df09ad28778f17035b76abb3b22")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7016777070:AAFRDJ6HCOzr0mOlqz9pakA2aSwN9JBtM1s") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7491863909:AAG0CGvq2xJweTsTTUzUt96lnlHM04dpwqM") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     ADMIN = int(os.environ.get("ADMIN", "1562935405"))
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "publicforward-bot")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "publicforwarrd-bot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1562935405').split()]
     FORCE_SUB_ID = environ.get('FORCE_SUB_ID', '-1002224312828')
     VERIFY_PIC = environ.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
