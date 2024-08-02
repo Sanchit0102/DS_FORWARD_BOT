7c515ada53a89402a34125446cb3beb83675e184
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-08-02 16:29:35 +0530

    Update Fsub.py

--- plugins/Fsub.py
+++ plugins/Fsub.py
@@ -3,16 +3,16 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
 from config import Config
 
-async def checkSub(bot, message):
+async def checkSub(client, message):
     userid = message.from_user.id
     try:
-        user =await bot.get_chat_member(Config.FORCE_SUB_ID, userid)
+        user =await client.get_chat_member(Config.FORCE_SUB_ID, userid)
         if user.status == enums.ChatMemberStatus.BANNED:
             await message.reply_text("Sorry. You're Banned. Contact my [Support Group](https://www.google.com) to get unbanned.")
             return False
         return True
     except UserNotParticipant:
-        invite_link = await bot.export_chat_invite_link(Config.FORCE_SUB_ID)
+        invite_link = await client.export_chat_invite_link(Config.FORCE_SUB_ID)
         join_button = InlineKeyboardButton("Join Channel", url=invite_link)
         join = InlineKeyboardMarkup([[join_button]])
         await message.reply_text("**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**", reply_markup=join)
